872a72c7457b2579c4d26fb791ae2f3948ffc413
Noel Sanchez <noel.sanchez@logicstudio.net>
2025-01-01 13:33:14 -0500

    Ajuste de ViteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist' // Asegúrate de que esto esté definido
-    }
+  plugins: [react()],
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true 
+  }
 });
