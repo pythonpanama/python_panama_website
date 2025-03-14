be0c17917d1edbfd435592118155c340175a82d3
NoelSanz25 <noelsanz8@gmail.com>
2025-03-13 23:15:27 -0500

    Update(ccs CodigoConducta and add classname custom-list)

--- src/components/code-conduct/CodigoConductaComponent.tsx
+++ src/components/code-conduct/CodigoConductaComponent.tsx
@@ -20,30 +20,30 @@ export function CodigoConductaComponent() {
                                 <article className="post post-large blog-single-post border-0 m-0 p-0">
                                     <div className="post-content ml-0">
                                         <h2 className="font-weight-bold">Python Panamá</h2>
-                                        <p className="custom-paragraph">Grupo aprobado por la Python Software Foundation (PSF) 
+                                        <p className="custom-text-paragraph">Grupo aprobado por la Python Software Foundation (PSF) 
                                             organización miembro 501(c)(3) dedicada al avance de la tecnología de código abierto
                                              relacionada con el lenguaje de programación Python.</p>
 
-                                        <p className="custom-paragraph">En torno a la que se organizan eventos de distintos tipos, 
+                                        <p className="custom-text-paragraph">En torno a la que se organizan eventos de distintos tipos, 
                                             quiere asegurar que todas las personas que participen en dichos eventos
                                              o comunicaciones tengan una experiencia profesional y positiva de aprendizaje,
                                               colaboración u ocio. Para ello, se espera que quien participe en la comunidad
                                                muestre respeto y cortesía hacia todos los demás.</p>
                                                
-                                        <p className="custom-paragraph">Al participar en la comunidad de Python Panamá, 
+                                        <p className="custom-text-paragraph">Al participar en la comunidad de Python Panamá, 
                                             te comprometes a fomentar una experiencia libre de acoso para todo el mundo, 
                                             independientemente de la edad, dimensión corporal, discapacidad visible o invisible, 
                                             etnicidad, características sexuales, identidad y expresión de género, nivel de experiencia,
                                              educación, nivel socio-económico, nacionalidad, apariencia personal, raza, religión,
                                               o identidad u orientación sexual.</p>
 
-                                        <p className="custom-paragraph">Este Código de Conducta detalla qué comportamientos se esperan, 
+                                        <p className="custom-text-paragraph">Este Código de Conducta detalla qué comportamientos se esperan, 
                                             cuáles se rechazan y qué mecanismos hay para ayudar a una persona
                                             que esté siendo objeto de comportamientos inadecuados.</p>
 
-                                        <p className="custom-paragraph">El Código de Conducta es un documento vivo y se actualizará cuando y si se considera necesario.</p>
+                                        <p className="custom-text-paragraph">El Código de Conducta es un documento vivo y se actualizará cuando y si se considera necesario.</p>
                                         
-                                        <p className="custom-paragraph">El Código de Conducta no es un “código” en el sentido de ser un algoritmo o un programa de computadora. 
+                                        <p className="custom-text-paragraph">El Código de Conducta no es un “código” en el sentido de ser un algoritmo o un programa de computadora. 
                                             El Código de conducta no se ejecuta "a ciegas y algorítmicamente", 
                                             sino que lo hacen cumplir los seres humanos que toman decisiones reales
                                              basadas en toda la información disponible y utilizando todo el contexto disponible.</p>
@@ -52,9 +52,9 @@ export function CodigoConductaComponent() {
                                             ¿Por qué un código de conducta?   
                                         </h4>
 
-                                        <p className="custom-paragraph">Siguiendo con el zen de Python, explícito mejor que implícito. 
+                                        <p className="custom-text-paragraph">Siguiendo con el zen de Python, explícito mejor que implícito. 
                                             Explicamos qué se espera del ambiente en cualquier evento de Python Panamá: </p>
-                                            <ul>
+                                            <ul className="custom-list">
                                                 <li>Favorece que más personas sepan que son bienvenidas.</li>
                                                 <li>Evita ambigüedades.</li>
                                                 <li>Construye un clima de confianza, donde si alguien quiere reportar un incidente, sabrá que no empezaremos por cuestionarle (<em>victim blaming</em>).</li>
@@ -64,7 +64,7 @@ export function CodigoConductaComponent() {
                                             Alcance
                                         </h4>
                                         
-                                        <p className="custom-paragraph">Este código de conducta es aplicable a todas las personas que participen
+                                        <p className="custom-text-paragraph">Este código de conducta es aplicable a todas las personas que participen
                                              en espacios de la comunidad de Python Panamá, ya sean en línea o presenciales. 
                                              También se aplica a espacios públicos donde una persona esté en representación de la comunidad. 
                                              Ejemplos de esto último incluyen el uso de la cuenta oficial de correo electrónico, 
@@ -75,8 +75,8 @@ export function CodigoConductaComponent() {
                                                 Nuestros estándares
                                             </h4>
 
-                                            <p className="custom-paragraph">Ejemplos de comportamiento que contribuyen a crear un ambiente positivo para nuestra comunidad:</p>
-                                            <ul>
+                                            <p className="custom-text-paragraph">Ejemplos de comportamiento que contribuyen a crear un ambiente positivo para nuestra comunidad:</p>
+                                            <ul className="custom-list">
                                                 <li>Demostrar empatía y amabilidad ante otras personas. No insultes o humilles a otros asistentes. Recuerda que las bromas sexistas, racistas o discriminatorias no son apropiadas. Nunca lo son.</li>
                                                 <li>Respetar las diferentes opiniones, puntos de vista y experiencias</li>
                                                 <li>Dar y aceptar adecuadamente críticas constructivas.</li>
@@ -86,8 +86,8 @@ export function CodigoConductaComponent() {
                                                 <li>Prestar especial atención a las personas que recién llegan a la comunidad.</li>
                                             </ul>
 
-                                            <p className="custom-paragraph">Ejemplos de comportamiento inaceptable:</p>
-                                            <ul>
+                                            <p className="custom-text-paragraph">Ejemplos de comportamiento inaceptable:</p>
+                                            <ul className="custom-list">
                                                 <li>El uso de lenguaje o imágenes sexualizadas, y aproximaciones o atenciones sexuales de cualquier tipo.</li>
                                                 <li>Comentarios despectivos (<em>trolling</em>), insultantes o derogatorios, y ataques personales o políticos.</li>
                                                 <li>Bromas racistas, sexistas o excluyentes.</li>
@@ -96,7 +96,7 @@ export function CodigoConductaComponent() {
                                                 <li>Otras conductas que puedan ser razonablemente consideradas como inapropiadas en un entorno profesional.</li>
                                             </ul>
 
-                                            <p className="custom-paragraph">Por acoso se entiende comentarios ofensivos relacionados con género, 
+                                            <p className="custom-text-paragraph">Por acoso se entiende comentarios ofensivos relacionados con género, 
                                                 orientación sexual, discapacidad, apariencia física, tamaño corporal,
                                                  etnia o religión, pornografía en espacios públicos, intimidación deliberada, 
                                                  acecho, persecución, acoso por fotografías o grabaciones, constante interrupción de charlas u otros eventos, 
@@ -106,7 +106,7 @@ export function CodigoConductaComponent() {
                                                 Cumplimiento
                                             </h4>
 
-                                            <p className="custom-paragraph">La administración de la comunidad es responsable de aclarar
+                                            <p className="custom-text-paragraph">La administración de la comunidad es responsable de aclarar
                                                  y hacer cumplir este código de conducta; 
                                                  en caso de que se determine un comportamiento inadecuado, 
                                                  tomará las acciones que considere oportunas.
@@ -124,11 +124,11 @@ export function CodigoConductaComponent() {
                                                 Denuncia e información de contacto
                                             </h4>
 
-                                            <p className="custom-paragraph">Los casos de comportamiento abusivo, 
+                                            <p className="custom-text-paragraph">Los casos de comportamiento abusivo, 
                                                 acosador o inaceptable de otro modo podrán ser denunciados 
                                                 a las personas administradoras de la comunidad responsables del cumplimiento:
                                             </p>
-                                            <ul>
+                                            <ul className="custom-list">
                                                 <li>Si es un evento presencial, ponte en contacto directamente con las personas organizadoras del evento. 
                                                     Es muy probable que hayan publicando un código de conducta específico del evento 
                                                     con instrucciones de a quién acudir; te proporcionarán un espacio seguro para ayudarte.</li>
@@ -138,16 +138,16 @@ export function CodigoConductaComponent() {
                                                     la Junta Directiva en <a href="mailto:contacto@es.python.org" target="_blank" rel="nofollow">contacto@pythonpanama.org</a>.</li>
                                             </ul>
 
-                                            <p className="custom-paragraph">Todas las personas administradoras de la comunidad están obligadas a 
+                                            <p className="custom-text-paragraph">Todas las personas administradoras de la comunidad están obligadas a 
                                                 respetar la privacidad y la seguridad de quienes denuncien incidentes.
                                             </p>
 
-                                            <p className="custom-paragraph font-weight-bold"> Esperamos que los participantes, conferencistas, 
+                                            <p className="custom-text-paragraph font-weight-bold"> Esperamos que los participantes, conferencistas, 
                                                 socios y patrocinadores sigan estas reglas en todos los espacios virtuales 
                                                 y presenciales de los eventos.
                                             </p>
 
-                                            <p className="custom-paragraph">Tenemos participantes encantadores y confiamos en que este código de conducta 
+                                            <p className="custom-text-paragraph">Tenemos participantes encantadores y confiamos en que este código de conducta 
                                                 refleja las opiniones de la mayoría de nuestros participantes.
                                             </p>
 
@@ -155,7 +155,7 @@ export function CodigoConductaComponent() {
                                                 Atribución
                                             </h4>
 
-                                            <p className="custom-paragraph">Este código de conducta extiende el ya existente con aportaciones de otros códigos:</p>
+                                            <p className="custom-text-paragraph">Este código de conducta extiende el ya existente con aportaciones de otros códigos:</p>
 
                                             <li>La versión 2.0 en español del <a href="https://www.contributor-covenant.org/" target="_blank" rel="nofollow"> Contributor Covenant</a>, 
                                                 versión 2.0 <a href="https://www.contributor-covenant.org/es/version/2/0/code_of_conduct/" target="_blank" rel="nofollow">HomePage</a> 
