e1deac6c2e31e50c7c475e83ab268b3836519571
NoelSanz25 <noelsanz8@gmail.com>
2025-05-01 19:51:36 -0500

    Modify(delete extra spaces)

--- src/components/common/BlogComponent.tsx
+++ src/components/common/BlogComponent.tsx
@@ -19,7 +19,6 @@ export function BlogComponent() {
 
             <div className="container px-4 py-5" id="custom-cards">
                     {/* <h2 className="pb-2 border-bottom">Custom cards</h2> */}
-
                 <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
                     
                     <div className="col">
